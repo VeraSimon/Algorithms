b7dc6b695cb32c798fb9d8f718e4990fe7d0624d
Vera Simon <vera.simon01@gmail.com>
2019-02-20 13:23:30 -0800

    Stock prices Came up with an iterative solution with a slightly dirty initialization

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,7 +4,43 @@ import argparse
 
 
 def find_max_profit(prices):
-    pass
+    # Write a function `find_max_profit` that receives as input a list of stock
+    # prices. Your function should return the maximum profit that can be made
+    # from a single buy and sell. You must buy first before selling; no
+    # shorting is allowed here.
+
+    # For example, `find_max_profit([1050, 270, 1540, 3800, 2])` should return
+    # 3530, which is the maximum profit that can be made from a single buy and
+    # then sell of these stock prices.
+
+    # [1050, 270, 1540, 3800, 2]
+    # 1050:
+    #     270 - 1050 = -780
+    #     1540 - 1050 = 490
+    #     3800 - 1050 = 2750
+    #     2 - 1050 = -1048
+    # 270:
+    #     1540 - 270 = 1270
+    #     3800 - 270 = 3530
+    #     2 - 270 = -268
+    # 1540:
+    #     3800 - 1540 = 2260
+    #     2 - 1540 = -1538
+    # 3800:
+    #     2 - 3800 = -3798
+
+    # Attempt 1: iterative
+    # O(n log n)? It's passing over (almost all of) the array once in the outer
+    # loop, then the sub array gets shorter by one each iteration of the outer
+    # loop.
+    # The `profit` initialization value gets checked twice, but :shrug:
+    profit = prices[1] - prices[0]
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            if prices[j] - prices[i] > profit:
+                profit = prices[j] - prices[i]
+
+    return profit
 
 
 if __name__ == '__main__':
