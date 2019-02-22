b207e35696dd455b6d38b5c7148fa04cc00bb4cf
Vera Simon <vera.simon01@gmail.com>
2019-02-21 19:04:51 -0800

    Making change Still WIP. Trying to work out the finer details of this final else statement.

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,27 +2,27 @@
 
 import sys
 
+# Imagine a small child ascending a staircase that has `n` steps. The child
+# can hop either 1, 2, or 3 steps at a time. Implement a function
+# `climbing_stairs` that counts the number of possible ways in which the
+# child can ascend the staircase.
+
+# For example, for a staircase with `n = 3` (the stair has 3 steps), there
+# are 4 possible ways for the child to ascend the staircase:
+
+# 1. They can jump 3 hops of 1 step
+#   n // 1 == n
+# 2. They can go up 1 step, followed by a jump of 2 steps
+#   1 + n // 2 == n
+# 3. They can jump up 2 steps, then go up the last step
+#   n // 2 + 1 == n
+# 4. They can make a single jump of 3 steps
+#   n // 3 == n
+
+# Thus, `climbing_stairs(3)` should return an answer of 4.
+
 
 def climbing_stairs(n, cache=None):
-    # Imagine a small child ascending a staircase that has `n` steps. The child
-    # can hop either 1, 2, or 3 steps at a time. Implement a function
-    # `climbing_stairs` that counts the number of possible ways in which the
-    # child can ascend the staircase.
-
-    # For example, for a staircase with `n = 3` (the stair has 3 steps), there
-    # are 4 possible ways for the child to ascend the staircase:
-
-    # 1. They can jump 3 hops of 1 step
-    #   n // 1 == n
-    # 2. They can go up 1 step, followed by a jump of 2 steps
-    #   1 + n // 2 == n
-    # 3. They can jump up 2 steps, then go up the last step
-    #   n // 2 + 1 == n
-    # 4. They can make a single jump of 3 steps
-    #   n // 3 == n
-
-    # Thus, `climbing_stairs(3)` should return an answer of 4.
-
     # Attempt 1: recursive (small)
     # O(~n**3)? Not quite n, but we're calling the function 3 times.
     # if n <= 0:
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,59 @@
 
 import sys
 
+# One day one of the wealthiest and also most eccentric patrons of the bank
+# walks up to your stall. They hand you some cash and tell you they want you to
+# figure out exactly how many ways there are to make change for the amount of
+# money they plopped down in front of you using only pennies, nickels, dimes,
+# quarters, and half-dollars.
+
+# Since this is a bank, you have an infinite supply of coinange. Write a
+# function `making_change` that receives as input an amount of money in cents
+# as well as an array of coin denominations and calculates the total number of
+# ways in which change can be made for the input amount using the given coin denominations.
+
+# For example, `making_change(10)` should return 4, since there are 4 ways to make change
+# for 10 cents using pennies, nickels, dimes, quarters, and half-dollars:
+
+#  1. We can make change for 10 cents using 10 pennies
+#  2. We can use 5 pennies and a nickel
+#  3. We can use 2 nickels
+#  4. We can use a single dime
+
+
 def making_change(amount, denominations):
-  pass 
+    cache = {}
+
+    def change_calc(amount_left):
+        # else:
+        #     steps = climbing_stairs(
+        #         n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache)
+        #     cache[n] = steps
+        #     # print(f"step {n}: {cache[n]}")
+        #     return steps
+
+        if amount_left <= 4:
+            return 1
+        elif amount_left in cache:
+            return cache[amount_left]
+        else:
+            ways = 0
+            for i in denominations:
+                print(i)
+                ways += change_calc(amount_left - i)
+                print(ways)
+            return ways
+
+    return change_calc(amount)
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test out your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
